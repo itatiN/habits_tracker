a8e69230d7b88e7371b03df1787507a1a1728ded
itatiN <itaticontato@gmail.com>
2023-01-20 17:23:51 -0300

    add: Loading Screen

--- FrontEnd/mobile/App.tsx
+++ FrontEnd/mobile/App.tsx
@@ -1,11 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
+import { Loading } from './src/components/Loading';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_800ExtraBold
+  });
+
+  if(!fontsLoaded) return <Loading/>;
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Estou deveras feliz por estar funcionanto!</Text>
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
     </View>
   );
 }
@@ -13,8 +23,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor:"#09090A",
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    color:'#FFF',
+    fontFamily: 'Inter_800ExtraBold'
+  }
 });
