b9c23cfa714f7c8058e53b75503543dc9eed5d2c
itatiN <itaticontato@gmail.com>
2023-01-20 20:35:34 -0300

    add: Get Possible/Completed Habit

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -26,9 +26,51 @@ export async function appRoutes(app: FastifyInstance) {
                             week_day: weekDays,
                         }
                     })
-                }   
+                }
             }
         })
+    })
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        const possibleHabits = await prisma.habit.findMany({
+            where:{
+                create_at:{
+                    lte: date,
+                },
+                WeekDays:{
+                    some:{
+                        week_day: weekDay,
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where:{
+                date: parsedDate.toDate()
+            },
+            include:{
+                dayHabit:true,
+            }
+        })
+
+        const completedHabits = day?.dayHabit.map(dayHabit =>{
+            return dayHabit.habit_id
+        })
+
+        return{
+            possibleHabits, 
+            completedHabits
+        }
     })
 
 }
