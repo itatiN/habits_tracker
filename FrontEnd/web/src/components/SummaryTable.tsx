f2e7275535b0c6fb64e55c0b1a4bfcca7be2aaea
itatiN <itaticontato@gmail.com>
2023-01-21 11:04:43 -0300

    add: Habit Days Placeholder

--- FrontEnd/web/src/components/SummaryTable.tsx
+++ FrontEnd/web/src/components/SummaryTable.tsx
@@ -4,6 +4,8 @@ import { HabitDay } from "./HabitDay"
 const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
 
 const summaryDates = generateRangeDatesFromYearStart()
+const minimumSummaryDatesSize = 18 * 7
+const amountOfDaysToFill = minimumSummaryDatesSize - summaryDates.length
 
 export function SummaryTable() {
     return (
@@ -23,9 +25,18 @@ export function SummaryTable() {
             </div>
 
             <div className="grid grid-rows-7 grid-flow-col  gap-3">
-                {summaryDates.map((date)=>{
-                    return(
-                        <HabitDay key={date.toString()}/>
+                {summaryDates.map((date) => {
+                    return (
+                        <HabitDay key={date.toString()} />
+                    )
+                })}
+                {amountOfDaysToFill > 0 && Array.from({ length: amountOfDaysToFill }).map((_,i) => {
+                    return (
+                        <div
+                            key={i}
+                            className="w-10 h-10 bg-zinc-900 border-2 border-zinc-800 rounded-lg
+                            opacity-40 cursor-not-allowed">
+                        </div>
                     )
                 })}
             </div>
