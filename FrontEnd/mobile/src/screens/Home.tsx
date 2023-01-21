e2192c3c26462a4f1a95abce8ce298298f21ccea
itatiN <itaticontato@gmail.com>
2023-01-21 12:47:58 -0300

    add: Mobile HabitDays

--- FrontEnd/mobile/src/screens/Home.tsx
+++ FrontEnd/mobile/src/screens/Home.tsx
@@ -1,10 +1,34 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 import { Header } from "../components/Header";
+import { HabitDay, DAY_SIZE } from "../components/HabitDay";
+import { generateRangeDatesFromYearStart } from '../utils/generate-range-between-dates'
+
+const weekDays = ['D', 'S', 'T', 'Q', 'Q', 'S', 'S']
+const datesFromYearStart = generateRangeDatesFromYearStart();
 
 export function Home() {
     return (
         <View className="flex-1 bg-background px-8 pt-16">
-            <Header/>
+            <Header />
+
+            <View className="flex-row mt-6 mb-2">
+                {weekDays.map((weekDays, i) => (
+                    <Text
+                        key={`${weekDays}-${i}`}
+                        className="text-zinc-400 text-xl font-bold text-center mx-1"
+                        style={{ width: DAY_SIZE }}
+                    >
+                        {weekDays}
+                    </Text>
+                ))}
+            </View>
+
+            <View className="flex-row flex-wrap">
+                {datesFromYearStart.map(date => (
+                    <HabitDay key={date.toISOString()} />
+                ))}
+            </View>
+
         </View>
     )
 }
