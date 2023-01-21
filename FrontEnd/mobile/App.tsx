cb6adf45de2efc216f05b8823317ba8b5ba7385b
itatiN <itaticontato@gmail.com>
2023-01-21 13:00:15 -0300

    add: Mobile Lib dayJS

--- FrontEnd/mobile/App.tsx
+++ FrontEnd/mobile/App.tsx
@@ -2,6 +2,7 @@ import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
 import { Home } from './src/screens/Home';
+import './src/libs/dayjs'
 
 export default function App() {
   const [fontsLoaded] = useFonts({
