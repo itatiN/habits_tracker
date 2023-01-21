a1d5f039449b904d6a58ddb7aa172c27b25d0d68
itatiN <itaticontato@gmail.com>
2023-01-21 12:23:14 -0300

    add: Mobile Logo and Button

--- FrontEnd/mobile/App.tsx
+++ FrontEnd/mobile/App.tsx
@@ -1,6 +1,7 @@
-import { StyleSheet, Text, View, StatusBar} from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_800ExtraBold } from '@expo-google-fonts/inter';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -10,25 +11,13 @@ export default function App() {
     Inter_800ExtraBold
   });
 
-  if(!fontsLoaded) return <Loading/>;
+  if (!fontsLoaded) return <Loading />;
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Estou deveras feliz por estar funcionanto!</Text>
-      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent/>
-    </View>
+    <>
+      <Home />
+      <StatusBar barStyle="light-content" backgroundColor="transparent" translucent />
+    </>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor:"#09090A",
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    color:'#FFF',
-    fontFamily: 'Inter_800ExtraBold'
-  }
-});
